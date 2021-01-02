ca3fa0d9a28227959d269e3c7226466d7ee14ffa
William Wu <dev.wwu@gmail.com>
2021-01-02 10:47:59 -0800

    Refactor grapher into MainView class

--- grapher.py
+++ grapher.py
@@ -2,124 +2,140 @@ import sys
 import json
 import tkinter as tk
 
-courses_dict = {}
+class MainView(tk.Frame):
+    def __init__(self, courses, depts, master=None):
+        tk.Frame.__init__(self, master)
+        self.master.title("Course Grapher")
+        self.grid() # row 0, col 0 in top-level window
+        self.create_widgets()
+        self.courses = courses
+        self.depts = depts
+        self.display_courses()
+        self.mouse_drag = False
+        self.drag_start = None
+        self.bind_listeners()
 
-# read from courses.json
-try:
-    with open("courses.json", "r") as file:
-        courses_dict = json.load(file)
-except FileNotFoundError:
-    sys.exit("Could not find courses.json")
-except json.JSONDecodeError:
-    sys.exit("Malformed JSON in courses.json")
+    def create_widgets(self):
+        self.canvas = tk.Canvas(self, width=1000, height=800,
+                                background="#ffffff")
+        self.canvas.grid(row=0, column=0)
+        self.panel = tk.Frame(self, width=250, height=800, background="#f0f0f0")
+        self.panel.grid(row=0, column=1)
 
-# group by dept
-depts = {}
-for course in courses_dict:
-    course_dept = courses_dict[course]["dept"]
-    if course_dept in depts:
-        depts[course_dept].append(course)
-    else:
-        depts[course_dept] = [course]
+    def display_courses(self):
+        curr_x, curr_y = 20, 50
+        max_y = 760
+        curr_letter = "A"
+        def next_column():
+            nonlocal curr_x, curr_y
+            curr_x += 100
+            curr_y = 50
+        def draw_col_letter():
+            self.canvas.create_text(curr_x - 10, 20, text=curr_letter,
+                                    anchor=tk.NW, font=("Arial", 30))
 
-# set up canvas
-window = tk.Tk()
-window.title("Course Grapher")
-canvas = tk.Canvas(window, width=1000, height=800, background="white")
-canvas.pack()
+        draw_col_letter()
+        for dept in sorted(list(self.depts)):
+            if dept[0] != curr_letter:
+                curr_letter = dept[0]
+                next_column()
+                draw_col_letter()
+            elif curr_y > max_y - 50:
+                next_column()
+                
+            curr_y += 10
+            self.canvas.create_text(curr_x - 10, curr_y, text=dept, anchor=tk.W,
+                                    font=("Arial", 20))
+            curr_y += 12
+            for course in self.depts[dept]:
+                course_dict = self.courses[course]
+                size = 0
+                if "leadsto" in course_dict:
+                    size = len(course_dict["leadsto"])
+                r = MainView.radius_from_size(size)
+                delta_y = max(r, 4)
+                curr_y += delta_y
+                self.draw_course(curr_x, curr_y, r, course)
+                curr_y += delta_y + 2
+                if curr_y > max_y:
+                    next_column()
 
-def draw_circle(x, y, radius, color="#78b6f5", hover_color="#659acf", tags=None):
-    canvas.create_oval(x - radius, y - radius, x + radius, y + radius,
-                       fill=color, activefill=hover_color, outline="",
-                       tags=tags)
+    def bind_listeners(self):
+        self.canvas.bind("<Button-1>", self.mouse_down)
+        self.canvas.bind("<Motion>", self.mouse_move)
+        self.canvas.bind("<ButtonRelease-1>", self.mouse_up)
 
-def draw_course(x, y, radius, label):
-    draw_circle(x, y, radius=radius, tags=label)
-    canvas.create_text(x + radius + 2, y, text=label, anchor=tk.W,
-                       font=("Arial", 10))
+    def draw_circle(self, x, y, radius, color="#78b6f5", hover_color="#659acf",
+                    tags=None):
+        self.canvas.create_oval(x - radius, y - radius, x + radius, y + radius,
+                                fill=color, activefill=hover_color, outline="",
+                                tags=tags)
 
-def radius_from_size(size):
-    if size <= 20:
-        return size + 2
-    elif size < 200:
-        return 0.32 * (size - 200) + 80
-    else:
-        return 80
+    def draw_course(self, x, y, radius, label):
+        self.draw_circle(x, y, radius=radius, tags=label)
+        self.canvas.create_text(x + radius + 2, y, text=label, anchor=tk.W,
+                                font=("Arial", 10))
 
-mouse_drag = False
-drag_start = None
-
-def mouse_down(event):
-    mouse_x, mouse_y = event.x, event.y
-    selected = canvas.find_overlapping(mouse_x, mouse_y,
-                                       mouse_x + 1, mouse_y + 1)
-    if len(selected) > 0:
-        tags = canvas.gettags(selected[0])
-        if len(tags) > 1:
-            # display_course(tags[0] + " " + tags[1])
-            print(tags)
+    def radius_from_size(size):
+        if size <= 20:
+            return size + 2
+        elif size < 200:
+            return 0.32 * (size - 200) + 80
         else:
-            start_drag(mouse_x, mouse_y)
-    else:
-        start_drag(mouse_x, mouse_y)
+            return 80
 
-def start_drag(x, y):
-    global mouse_drag, drag_start
-    mouse_drag = True
-    drag_start = (x, y)
+    def mouse_down(self, event):
+        mouse_x, mouse_y = event.x, event.y
+        def start_drag():
+            self.mouse_drag = True
+            self.drag_start = (mouse_x, mouse_y)
+        selected = self.canvas.find_overlapping(mouse_x, mouse_y,
+                                                mouse_x + 1, mouse_y + 1)
+        if len(selected) > 0:
+            tags = self.canvas.gettags(selected[0])
+            if len(tags) > 1:
+                # display_course(tags[0] + " " + tags[1])
+                print(tags)
+            else:
+                start_drag()
+        else:
+            start_drag()
 
-def mouse_move(event):
-    global mouse_drag, drag_start
-    if mouse_drag:
-        dx = event.x - drag_start[0]
-        dy = event.y - drag_start[1]
-        canvas.move("all", dx, dy)
-        drag_start = (event.x, event.y)
+    def mouse_move(self, event):
+        if self.mouse_drag:
+            dx = event.x - self.drag_start[0]
+            dy = event.y - self.drag_start[1]
+            self.canvas.move("all", dx, dy)
+            self.drag_start = (event.x, event.y)
 
-def mouse_up(event):
-    global mouse_drag
-    mouse_drag = False
+    def mouse_up(self, event):
+        self.mouse_drag = False
 
-# display all courses
-curr_x, curr_y = 20, 50
-max_y = 760
-curr_letter = "A"
-def next_column():
-    global curr_x, curr_y
-    curr_x += 100
-    curr_y = 50
-def col_letter(letter):
-    canvas.create_text(curr_x - 10, 20, text=letter, anchor=tk.NW,
-                       font=("Arial", 30))
+def read_courses():
+    courses = None
 
-col_letter(curr_letter)
-for dept in sorted(list(depts)):
-    if dept[0] != curr_letter:
-        next_column()
-        curr_letter = dept[0]
-        col_letter(curr_letter)
-    elif curr_y > max_y - 50:
-        next_column()
-        
-    curr_y += 10
-    canvas.create_text(curr_x - 10, curr_y, text=dept, anchor=tk.W,
-                       font=("Arial", 20))
-    curr_y += 12
-    for course in depts[dept]:
-        course_dict = courses_dict[course]
-        size = 0
-        if "leadsto" in course_dict:
-            size = len(course_dict["leadsto"])
-        r = radius_from_size(size)
-        delta_y = max(r, 4)
-        curr_y += delta_y
-        draw_course(curr_x, curr_y, r, course)
-        curr_y += delta_y + 2
-        if curr_y > max_y:
-            next_column()
+    # read from courses.json
+    try:
+        with open("courses.json", "r") as file:
+            courses = json.load(file)
+    except FileNotFoundError:
+        sys.exit("Could not find courses.json")
+    except json.JSONDecodeError:
+        sys.exit("Malformed JSON in courses.json")
 
-canvas.bind("<Button-1>", mouse_down)
-canvas.bind("<Motion>", mouse_move)
-canvas.bind("<ButtonRelease-1>", mouse_up)
+    return courses
 
-window.mainloop()
+def group_by_dept(courses):
+    depts = {}
+    for course in courses:
+        course_dept = courses[course]["dept"]
+        if course_dept in depts:
+            depts[course_dept].append(course)
+        else:
+            depts[course_dept] = [course]
+    return depts
+
+courses = read_courses()
+depts = group_by_dept(courses)
+app = MainView(courses, depts)
+app.mainloop()
