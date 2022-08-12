da91063430344866fd6a5d498c7e5b347bdd3c9b
William Wu <dev.wwu@gmail.com>
2022-08-12 10:43:46 -0700

    Follow links to course pages

--- scraping/catalog_spider.py
+++ scraping/catalog_spider.py
@@ -2,9 +2,14 @@ import scrapy
 
 
 class CatalogSpider(scrapy.Spider):
-    name = "catalog"
-    start_urls = ["https://catalog.ucsd.edu/front/courses.html"]
+    name = 'catalog'
+    start_urls = ['https://catalog.ucsd.edu/front/courses.html']
 
     def parse(self, response):
-        with open('test.html', 'wb') as f:
-            f.write(response.body)
+        selectors = response.xpath(
+            '//span[@class="courseFacLink"]/a[text()[contains(.,"courses")]]/@href')
+        for link in selectors.getall():
+            yield response.follow(link, callback=self.parse_courses)
+
+    def parse_courses(self, response):
+        print(response.xpath('//h1/text()').get())
