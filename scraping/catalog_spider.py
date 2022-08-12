58bcf40e0afa1033abffec029c46bb2941a529d2
William Wu <dev.wwu@gmail.com>
2022-08-12 13:56:23 -0700

    Parse course codes, titles, and units

--- scraping/catalog_spider.py
+++ scraping/catalog_spider.py
@@ -1,4 +1,8 @@
+import re
 import scrapy
+
+course_info_matcher = re.compile(
+    r'^\s*(?P<code>.+?)\.\s*(?P<title>.+?)\s*\((?P<units>.+?)\)')
 
 
 class CatalogSpider(scrapy.Spider):
@@ -8,8 +12,31 @@ class CatalogSpider(scrapy.Spider):
     def parse(self, response):
         selectors = response.xpath(
             '//span[@class="courseFacLink"]/a[text()[contains(.,"courses")]]/@href')
+        i = 0
         for link in selectors.getall():
+            if i >= 20:
+                break
+            i += 1
             yield response.follow(link, callback=self.parse_courses)
 
     def parse_courses(self, response):
-        print(response.xpath('//h1/text()').get())
+        dept = self._department_from_url(response.url)
+        name_selectors = response.xpath('//p[@class="course-name"]/text()')
+        # description_selectors = response.xpath(
+        #     '//p[@class="course-name"]/following-sibling::p[@class="course-descriptions"][1]'
+        # ).xpath('string()')
+        for line in name_selectors.getall():
+            match = course_info_matcher.match(line)
+            if match is None:
+                continue
+            code, title, units = match.group('code', 'title', 'units')
+            yield {
+                'dept': dept,
+                'code': code,
+                'title': title,
+                'units': units
+            }
+
+    def _department_from_url(self, url):
+        start, end = url.rfind('/') + 1, url.rfind('.')
+        return url[start:end].upper()
