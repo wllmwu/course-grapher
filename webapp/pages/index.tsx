82a732541d9de72f776ff843da6d2abee91f5dd6
William Wu <dev.wwu@gmail.com>
2022-09-23 11:42:27 -0700

    Add quick links to home page

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Link from "next/link";
 import Page from "../components/Page";
 import SearchBar from "../components/SearchBar";
 import styles from "../styles/Home.module.css";
+
+const quickLinks = [
+  {
+    title: "Department List",
+    description: "Every department with courses in the catalog",
+    link: "/departments",
+  },
+  {
+    title: "General Catalog",
+    description: "Official online course catalog from UCSD",
+    link: "https://catalog.ucsd.edu/front/courses.html",
+  },
+  {
+    title: "TritonLink",
+    description: "The place to go for all our student needs",
+    link: "https://students.ucsd.edu",
+  },
+  {
+    title: "CAPE",
+    description: "Course And Professor Evaluations gathered every quarter",
+    link: "https://cape.ucsd.edu",
+  },
+];
 
 function Home() {
   return (
@@ -13,6 +37,21 @@ function Home() {
         course below or visit the list of departments to get started.
       </p>
       <SearchBar />
+      <h2>Quick Links</h2>
+      <ul className={styles.quickLinkList}>
+        {quickLinks.map(({ title, description, link }) => (
+          <li key={link} className={styles.quickLinkItem}>
+            <Link href={link}>
+              <a>
+                <div className={styles.quickLinkBox}>
+                  <h3>{title}</h3>
+                  <p>{description}</p>
+                </div>
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </Page>
   );
 }
