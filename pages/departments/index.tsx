5c17b9a23e36a2252b8f2bc739386854f4299b56
William Wu <dev.wwu@gmail.com>
2022-09-24 15:07:41 -0700

    Use link cards in departments page

--- pages/departments/index.tsx
+++ pages/departments/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import type { GetStaticProps } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import { promises as fs } from "fs";
 import path from "path";
 import type { Department } from "../../utils/data-schema";
 import Page from "../../components/Page";
+import styles from "../../styles/DepartmentsPage.module.css";
+import cardStyles from "../../styles/LinkCard.module.css";
 
 export const getStaticProps: GetStaticProps = async () => {
   const filePath = path.join(process.cwd(), "scraping/data/index.json");
@@ -21,17 +24,46 @@ interface DepartmentsPageProps {
 }
 
 function DepartmentsPage({ departments }: DepartmentsPageProps) {
+  const deptsByLetter: Record<string, Department[]> = {};
+  for (const dept of Object.values(departments)) {
+    const firstLetter = dept.code.charAt(0);
+    if (deptsByLetter.hasOwnProperty(firstLetter)) {
+      deptsByLetter[firstLetter].push(dept);
+    } else {
+      deptsByLetter[firstLetter] = [dept];
+    }
+  }
+  const letters = Object.keys(deptsByLetter);
+
   return (
     <Page>
       <Head>
         <title>Departments | GAPE</title>
       </Head>
       <h1>All Departments</h1>
-      {Object.values(departments).map((dept) => (
-        <div key={dept.code} id={dept.code}>
-          <h3>{`${dept.code} | ${dept.name}`}</h3>
+      <p>
+        This page lists all departments found in the catalog, ordered
+        alphabetically. Click on a department to view its courses.
+      </p>
+      <div className={styles.cardListWrapper}>
+        <div className={styles.cardList}>
+          {letters.map((letter) => (
+            <React.Fragment key={letter}>
+              <h2 id={letter}>{letter}</h2>
+              {deptsByLetter[letter].map((dept) => (
+                <Link key={dept.code} href={`/departments/${dept.code}`}>
+                  <a>
+                    <div className={`${cardStyles.card} ${styles.card}`}>
+                      <h3>{dept.code}</h3>
+                      <p>{dept.name}</p>
+                    </div>
+                  </a>
+                </Link>
+              ))}
+            </React.Fragment>
+          ))}
         </div>
-      ))}
+      </div>
     </Page>
   );
 }
