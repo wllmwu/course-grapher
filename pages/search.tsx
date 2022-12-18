6d11d355b2c80b16a8905f5ef79eab0dc43d095d
William Wu <dev.wwu@gmail.com>
2022-12-18 13:49:04 -0800

    Fix bug in search, fill search results from frontend cache

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,10 +1,11 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import type { GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import type { Course } from "../utils/data-schema";
 import { readDataDirectory } from "../utils/buildtime";
 import * as cache from "../utils/frontend-cache";
-import { deslugifyCourseCode } from "../utils";
+import { courseComparator, deslugifyCourseCode } from "../utils";
 import Page from "../components/Page";
 import SearchBar from "../components/SearchBar";
 import CourseListing from "../components/CourseListing";
@@ -33,32 +34,44 @@ const MIN_QUERY_LENGTH = 2;
 
 function SearchPage({ courseCodes }: SearchPageProps) {
   const router = useRouter();
-  const searchResults = useMemo<string[] | null>(() => {
-    // extract search query string
-    const query = router.query.q;
-    if (!query) {
-      return null;
-    }
-    let searchText: string;
-    if (typeof query === "string") {
-      searchText = query;
-    } else if (query.length > 0) {
-      searchText = query[0];
-    } else {
-      return null;
-    }
+  const [searchResults, setSearchResults] = useState<Course[] | null>(null);
+  useEffect(() => {
+    async function loadResults() {
+      // extract search query string
+      const query = router.query.q;
+      if (!query) {
+        return;
+      }
+      let searchText: string;
+      if (typeof query === "string") {
+        searchText = query;
+      } else if (query.length > 0) {
+        searchText = query[0];
+      } else {
+        return;
+      }
 
-    // validate and clean search query
-    if (searchText.length < MIN_QUERY_LENGTH) {
-      return null;
-    }
-    searchText = searchText.trim();
-    searchText = searchText.replaceAll(/[^A-z0-9\s]/g, "");
-    searchText = searchText.replaceAll(/\s+/g, " ");
-    searchText = searchText.toUpperCase();
+      // validate and clean search query
+      if (searchText.length < MIN_QUERY_LENGTH) {
+        return;
+      }
+      searchText = searchText.trim();
+      searchText = searchText.replaceAll(/[^A-z0-9\s]/g, "");
+      searchText = searchText.replaceAll(/\s+/g, " ");
+      searchText = searchText.toUpperCase();
 
-    // perform search
-    return searchCourseCodes(searchText, courseCodes);
+      // perform search
+      const resultCodes = await searchCourseCodes(searchText, courseCodes);
+      const fetchedResults = await Promise.all(
+        resultCodes.map((code) => cache.getCourse(code))
+      );
+      const results = fetchedResults.filter(
+        (value) => value !== null
+      ) as Course[];
+      results.sort(courseComparator);
+      setSearchResults(results);
+    }
+    loadResults();
   }, [courseCodes, router.query.q]);
 
   return (
@@ -81,7 +94,7 @@ function SearchPage({ courseCodes }: SearchPageProps) {
             <>
               <h2>Search results</h2>
               {searchResults.map((course) => (
-                <p key={course}>{course}</p>
+                <CourseListing key={course.code} course={course} />
               ))}
             </>
           )}
@@ -91,12 +104,12 @@ function SearchPage({ courseCodes }: SearchPageProps) {
   );
 }
 
-function searchCourseCodes(query: string, courseCodes: string[]) {
+async function searchCourseCodes(query: string, courseCodes: string[]) {
   let low = 0;
   let high = courseCodes.length;
   let mid = 0;
   let found = false;
-  while (low <= high) {
+  while (low < high) {
     mid = Math.floor((low + high) / 2);
     if (courseCodes[mid].startsWith(query)) {
       found = true;
