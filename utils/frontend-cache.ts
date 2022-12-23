c155b387c192b9f17a37bd04e13f83258b200a87
William Wu <dev.wwu@gmail.com>
2022-12-23 08:57:05 -0800

    Store pending Promises in frontend cache

--- utils/frontend-cache.ts
+++ utils/frontend-cache.ts
@@ -1,24 +1,38 @@
 import { Course } from "./data-schema";
 import { slugifyCourseCode } from ".";
 
-const courseCache = new Map<string, Course | null>();
+const courseCache = new Map<string, Promise<Course | null> | Course | null>();
 
 export async function getCourse(code: string) {
   if (!courseCache.has(code)) {
-    await loadCourse(code);
+    loadCourse(code);
   }
-  return courseCache.get(code) as Course | null;
+  return courseCache.get(code) as Promise<Course | null> | Course | null;
 }
 
-async function loadCourse(code: string) {
-  courseCache.set(code, await fetchFromStaticFiles(code));
+/**
+ * Asynchronously fetches the specified course, filling in the cache with the
+ * pending `Promise`. When the fetch completes, the result (either the course
+ * object or `null`) replaces the `Promise` in the cache and the `Promise`
+ * itself also fulfills with that result. With this logic, we avoid making a
+ * request for a course when other requests are already pending for the same
+ * course.
+ */
+function loadCourse(code: string) {
+  courseCache.set(
+    code,
+    fetchFromStaticFiles(code).then((result) => {
+      courseCache.set(code, result);
+      return result;
+    })
+  );
 }
 
 async function fetchFromStaticFiles(code: string) {
   try {
     const response = await fetch(`/data/${slugifyCourseCode(code)}.json`);
     if (!response.ok) {
-      console.error(`Error retrieving ${code}: status ${response.status}`);
+      // some 404s are expected because the data are not perfectly consistent
       return null;
     }
     const json = await response.json();
